9d74ad2e1ebbd11e4fe271e478d8ae8ea9c78423
RAJ TILAK SINGH <87015087+crackerghost@users.noreply.github.com>
2024-10-25 22:59:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
+function App() {
+  const [name, setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [result,setResult] = useState("");
+  axios.defaults.baseURL = "http://localhost:3500";
+
+  // useEffect(() => {
+  //   const getName = async () => {
+  //     try {
+  //       const res = await axios.post("/getName",{
+  //         name: "Raj",
+  //         age: 20,
+  //       }, {
+  //         headers: {
+  //           Authorization: "raj",
+  //           xcustom : "mycustom"
+  //         },
+  //         params: {
+  //           id: "123@",
+  //         },
+  //       });
+  //       setName(res.data.name);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   getName();
+  // }, [name]);
+
+  const handleSubmit = (e)=>{
+   e.preventDefault()
+   const postData = async () => {
+     try {
+        const res = await axios.post("/createUser",{
+          email: email,
+          password: password,
+        }
+        )
+        setResult(res.data.msg);
+      } catch (error) {
+        console.log(error);
+      }
+   }
+   postData()
+
+  }
+
+  return (
+    <div className="main">
+     <form onSubmit={handleSubmit}>
+
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" />
+        <p>{result}</p>
+        <button type="submit">Submit</button>
+     </form>
+    </div>
+  );
+}
+
+export default App;
